5d4b1c817ffdec9ea9986f85d7147cc838013679
beaterjy <13750344599@163.com>
2020-12-16 16:17:39 +0800

    Finish html and css.

---
+++ material/js/some.js
@@ -0,0 +1,3 @@
+document.write(unescape("%3Cspan id='_ideConac' %3E%3C/span%3E"));
+var span_msg = document.getElementById("_ideConac");
+span_msg.innerHTML = '<a href="//bszs.conac.cn/sitename?method=show&id=539CD6B4201A08F9E053022819AC3373" target="_blank"><img id="imgConac" vspace="0" hspace="0" border="0" src="//dcs.conac.cn/image/red.png" data-bd-imgshare-binded="1"></a>';
